896745a5d6553952a7aeb171cb2a14d358951c04
qasap <qa,sapaev@gmail.com>
2022-06-30 15:46:17 +0300

    Synhron

--- src/main/java/Lesson4/HomeTask5.java
+++ src/main/java/Lesson4/HomeTask5.java
@@ -7,11 +7,11 @@ import java.util.Scanner;
 
 public class HomeTask5 {
     public static void main(String[] args) throws IOException {
-        Scanner sc=new Scanner(System.in);
-        Random rn=new Random();
+        Scanner sc = new Scanner(System.in);
+        Random rn = new Random();
         System.out.println("Введите число количества элементов:");
-        int input=sc.nextInt();
-        if (input>3) {
+        int input = sc.nextInt();
+        if (input > 3) {
             if (input % 2 == 0) {
                 int[] array = new int[input];
                 int[] array1 = new int[input / 2];
@@ -25,9 +25,7 @@ public class HomeTask5 {
                 }
                 System.out.println(Arrays.toString(array));
                 System.out.println(Arrays.toString(array1));
-            }
-
-            else if (input % 2 != 0) {
+            } else if (input % 2 != 0) {
                 int[] array = new int[input];
                 int[] array1 = new int[(input + 1) / 2];
                 int j = 0;
@@ -43,9 +41,41 @@ public class HomeTask5 {
             }
 
 
-        }else {
-            System.out.println("Введенное число должно быть больше 3");
-        }
+        } else {
+            while (input <= 3) {
+                System.out.println("Введенное число должно быть больше 3. Попробуйте снова");
+                input = sc.nextInt();
+                if(input>3){if (input % 2 == 0) {
+                    int[] array = new int[input];
+                    int[] array1 = new int[input / 2];
+                    int j = 0;
+                    for (int i = 0; i < array.length; i++) {
+                        array[i] = rn.nextInt(0, input);
+                        if (i % 2 == 0) {
+                            array1[j] = array[i];
+                            j++;
+                        }
+                    }
+                    System.out.println(Arrays.toString(array));
+                    System.out.println(Arrays.toString(array1));
+                } else if (input % 2 != 0) {
+                    int[] array = new int[input];
+                    int[] array1 = new int[(input + 1) / 2];
+                    int j = 0;
+                    for (int i = 0; i < array.length; i++) {
+                        array[i] = rn.nextInt(0, input);
+                        if (i % 2 == 0) {
+                            array1[j] = array[i];
+                            j++;
+                        }
+                    }
+                    System.out.println(Arrays.toString(array));
+                    System.out.println(Arrays.toString(array1));
+                }
 
+                }
+            }
+
+        }
     }
 }
